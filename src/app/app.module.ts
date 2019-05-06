a9b7105d3d6f5ac7f41960d6c917a1b1d2b77995
markobojkic <marko.bojkic@uns.ac.rs>
2019-05-06 15:32:58 +0200

    Vezbe 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VoziloComponent } from './components/primer-components/vozilo/vozilo.component';
+import { AutomobilComponent } from './components/primer-components/automobil/automobil.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MatButtonModule, 
+         MatIconModule,
+         MatListModule,
+         MatSidenavModule,
+         MatGridListModule,
+         MatExpansionModule } from '@angular/material';
+import { HomeComponent } from './components/core/home/home.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { ArtiklComponent } from './components/artikl/artikl.component';
+import { DobavljacComponent } from './components/dobavljac/dobavljac.component';
+import { PorudzbinaComponent } from './components/porudzbina/porudzbina.component';
+import { StavkaPorudzbineComponent } from './components/stavka-porudzbine/stavka-porudzbine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent,
+    HomeComponent,
+    AboutComponent,
+    AuthorComponent,
+    ArtiklComponent,
+    DobavljacComponent,
+    PorudzbinaComponent,
+    StavkaPorudzbineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
